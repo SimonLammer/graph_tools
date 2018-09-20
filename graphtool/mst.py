81a2c2255a8e947b4a74f62d80aa38c939a5060c
GCoiffier <guillaume.coiffier.42@orange.fr>
2018-09-20 13:05:47 +0200

    mst functions backbone

---
+++ graphtool/mst.py
@@ -0,0 +1,15 @@
+# Minimum spanning tree algorithms
+
+def MST(graph,algo='Kruskal'):
+    if algo.lower()=='prim':
+        return Prim(graph)
+    elif algo.lower()=="kruskal":
+        return Kruskal(graph)
+    else:
+        raise Exception("algo should be 'Prim' or 'Kruskal'")
+
+def Kruskal(graph):
+    pass
+
+def Prim(graph):
+    pass
