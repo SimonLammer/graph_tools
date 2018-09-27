4f2fe161f222a881cacc1bb8ca54cb422bb707d6
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2018-09-27 11:08:52 +0200

    tests: find_isolated & density test

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,5 @@
 import pytest
-from graphtool.graph import *
+from graphtool.graph import Graph, Edge
 
 
 def test_test():
@@ -10,3 +10,14 @@ def test_graph_from_edge_list():
     graph1 = Graph.from_edge_list("graph_examples/triangle_edge_list.txt")
     graph2 = Graph.from_edge_list([Edge(0, 1), Edge(1, 2), Edge(2, 0)])
     print(graph1._dict)
+    print(graph2._dict)
+
+
+def test_find_isolated_vertices():
+    graph1 = Graph.from_edge_list("graph_examples/triangle_edge_list.txt")
+    assert graph1.find_isolated_vertices() == []
+
+
+def test_density():
+    graph1 = Graph.from_edge_list("graph_examples/triangle_edge_list.txt")
+    assert graph1.density() == 1
