b16cbc0ed0f4d0cafe4055a5154e27d1240936f2
GCoiffier <guillaume.coiffier.42@orange.fr>
2018-10-15 11:04:35 +0200

    fixed paths tests (for performance reasons)

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -3,31 +3,28 @@ import os
 from graphtool.graph import *
 from graphtool.algorithms import *
 
+graph_files = ["graph_100n_1000m.txt","triangle_edge_list.txt"]
 
 def test_shortest_path():
-    graph_files = os.listdir("graph_examples/")
-    for path in graph_files:
-        if ".txt" in path:
-            graph = Graph.from_edge_list("graph_examples/"+path)
-            adj = all_shortest_paths(graph)
-            N = len(adj)
-            for i in range(N):
-                for j in range(i):
-                    assert adj[i][j] == shortest_path(graph,
-                                                      Vertex(i),
-                                                      Vertex(j),
-                                                      lambda a, b: 1)[0]
+    for g in graph_files:
+        graph = Graph.from_edge_list("graph_examples/"+g)
+        adj = all_shortest_paths(graph)
+        N = len(adj)
+        for i in range(N):
+            for j in range(i):
+                assert adj[i][j] == shortest_path(graph,
+                                                  Vertex(i),
+                                                  Vertex(j),
+                                                  lambda a, b: 1)[0]
 
 
 def test_dijsktra():
-    graph_files = os.listdir("graph_examples/")
-    for path in graph_files:
-        if ".txt" in path:
-            graph = Graph.from_edge_list("graph_examples/"+path)
-            adj = all_shortest_paths(graph)
-            N = len(adj)
-            for i in range(N):
-                for j in range(i):
-                    assert adj[i][j] == dijkstra(graph,
-                                                 Vertex(i),
-                                                 Vertex(j))[0]
+    for g in graph_files:
+        graph = Graph.from_edge_list("graph_examples/"+g)
+        adj = all_shortest_paths(graph)
+        N = len(adj)
+        for i in range(N):
+            for j in range(i):
+                assert adj[i][j] == dijkstra(graph,
+                                             Vertex(i),
+                                             Vertex(j))[0]
