0594499499bbac67c7f391dc1f0c5bb7217e8516
GCoiffier <guillaume.coiffier.42@orange.fr>
2018-10-19 14:32:21 +0200

    improved Vertex tests

--- tests/test_vertex.py
+++ tests/test_vertex.py
@@ -4,12 +4,26 @@ from graphtool.graph import *
 
 def test_vertex_eq():
     vertex1 = Vertex(0)
+    vertex2 = Vertex(0)
     assert vertex1 == 0
+    assert vertex1 == vertex2
 
 
-def test_vertex_lt():
+def test_vertex_comp():
     vertex1 = Vertex(0)
+    vertex2 = Vertex(1)
     assert vertex1 < 3
+    assert vertex1 <= 3
+    assert vertex1 <= 0
+    assert vertex1 < vertex2
+    assert vertex1 <= vertex2
+    assert vertex1 <= vertex1
+    assert vertex2 > 0
+    assert vertex2 >= 0
+    assert vertex2 >= 1
+    assert vertex2 > vertex1
+    assert vertex2 >= vertex1
+    assert vertex2 >= vertex2
 
 
 def test_vertex_init():
